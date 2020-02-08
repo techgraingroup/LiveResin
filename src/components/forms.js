758b57c69c709c7757743a5febddd2e85b4c2df4
aldidas <aldi@daswanto.com>
2020-02-08 09:38:01 -0500

    form element - contact us

---
+++ src/components/forms.js
@@ -0,0 +1,72 @@
+import React, { useState, useRef } from "react"
+import styled from "styled-components"
+
+export const TextInput = styled(
+  ({ label, name, type, onChange, value, placeholder, className, ...rest }) => {
+    const [active, setActive] = useState(false)
+    const inputEl = useRef(null)
+    const eleClass = `${className} ${active && "active"}`
+    const toggleActive = stat => {
+      const currentValue = inputEl.current.value
+      if (!currentValue && stat === false) {
+        setActive(false)
+      } else {
+        setActive(true)
+      }
+    }
+    return (
+      <div {...rest} className={eleClass}>
+        <label htmlFor={name}>{label}</label>
+        {type === "textarea" ? (
+          <textarea
+            ref={inputEl}
+            rows={10}
+            cols={10}
+            name={name}
+            value={value}
+            onChange={onChange}
+            onFocus={() => toggleActive(true)}
+            onBlur={() => toggleActive(false)}
+          />
+        ) : (
+          <input
+            ref={inputEl}
+            name={name}
+            type={type || "text"}
+            value={value}
+            onChange={onChange}
+            onFocus={() => toggleActive(true)}
+            onBlur={() => toggleActive(false)}
+          />
+        )}
+      </div>
+    )
+  }
+)`
+  margin-bottom: 25px;
+  border: 1px solid #000;
+  position: relative;
+  padding: 0 24px;
+  &.active {
+    label {
+      transform: translate(0, 2px) scale(0.75);
+    }
+  }
+  label {
+    opacity: 0.3;
+    position: absolute;
+    font-size: 18px;
+    margin-top: 10px;
+    transform-origin: top left;
+    transform: translate(0, 16px) scale(1);
+    transition: all 0.1s ease-in-out;
+  }
+  input,
+  textarea {
+    border: none;
+    outline: 0;
+    font-size: 18px;
+    padding: 34px 0 12px;
+    width: 100%;
+  }
+`
--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useContext } from "react"
 import { Link } from "gatsby"
+import Grid from "styled-components-grid"
 import { AppContext } from "../context"
 import SEO from "../components/seo"
+import { Box } from "../components/box"
 import { PageTitle } from "../components/text"
+import { TextInput } from "../components/forms"
+import { Button } from "../components/button"
 
 const ContactUsPage = () => {
   const { dispatch } = useContext(AppContext)
@@ -16,6 +20,28 @@ const ContactUsPage = () => {
     <>
       <SEO title="Contact Us" />
       <PageTitle title={`Contact Us`} />
+      <Box top="0">
+        <Grid>
+            <Grid.Unit size={{ md: 1, lg: 3 / 5 }}>
+            <form method="GET" action="#">
+              <TextInput label="Your name" placeholder="Your name" />
+              <TextInput
+                label="Your email"
+                type="email"
+                placeholder="Your email"
+              />
+              <TextInput
+                type="textarea"
+                label="Message"
+                placeholder="Message"
+              />
+              <Button type="submit" color="#FFF" bgColor="#000">
+                Submit
+              </Button>
+            </form>
+          </Grid.Unit>
+        </Grid>
+      </Box>
     </>
   )
 }
