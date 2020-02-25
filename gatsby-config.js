b982b1442981f0c30ab8352817cdd0818e28cb89
aldidas <aldi@daswanto.com>
2020-02-25 09:36:08 -0500

    remove gatsby ffmpeg

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,6 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-transformer-ffmpeg`,
     {
       resolve: `gatsby-source-instagram`,
       options: {
