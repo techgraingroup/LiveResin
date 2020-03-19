5d9fbb9e475bb726481e9cc4c6eafed66538113d
aldidas <aldi@daswanto.com>
2020-03-19 00:17:30 -0400

    storelocator data updated

--- src/pages/store-locator.js
+++ src/pages/store-locator.js
@@ -15,48 +15,84 @@ const {
   breakpoints: { md, xl },
 } = THEME
 
-const Store = styled.a`
-  display: block;
+const StateName = styled.h2`
+  font-family: MontHeavy, sans-sarif;
+  letter-spacing: -0.035em;
+  margin: 0;
+  border-top: 4px solid #000;
   border-bottom: 4px solid #000;
-  color: #000;
-  text-decoration: none;
-  h2 {
-    font-family: MontHeavy, sans-serif;
-    margin-top: 0;
-    margin-bottom: 0;
-    line-height: 105.8%;
-    padding-top: 30px;
-    ${fluidRange(
-      {
-        prop: "font-size",
-        fromSize: "30px",
-        toSize: "48px",
-      },
-      `${md}px`,
-      `${xl}px`
-    )}
+  font-size: 30px;
+  height: 80px;
+  line-height: 80px;
+  @media only screen and (min-width: ${md}px) {
+    font-size: 48px;
+    height: 100px;
+    line-height: 100px;
   }
-  &:first-child {
-    h2 {
-      padding-top: 0;
+  @media only screen and (max-width: ${md}px) {
+    &.first {
+      border-top: 0;
+      margin-top: -10px;
     }
   }
-  p {
-    margin-bottom: 25px;
+`
+
+const LocationBlock = styled.div`
+  border-top: 1px solid #000;
+  &.first {
+    border-top: 0;
+  }
+  padding: 25px 0;
+  @media only screen and (min-width: ${md}px) {
+    padding: 35px 0;
+  }
+`
+
+const LocationName = styled.h3`
+  font-family: MontHeavy, sans-sarif;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  margin: 0 0 25px 0;
+  font-size: 20px;
+  @media only screen and (min-width: ${md}px) {
+    margin: 0 0 35px 0;
+    font-size: 24px;
+  }
+`
+
+const StoreList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  li {
+    list-style: none;
+    a {
+      font-size: 16px;
+      font-family: MontHeavy, sans-sarif;
+      text-decoration: none;
+      color: #000;
+      line-height: 150%;
+    }
   }
 `
 
 const StoreLocatorPage = () => {
   const { dispatch } = useContext(AppContext)
   const {
-    dataJson: { storeLocator },
+    dataJson: { stores },
   } = useStaticQuery(graphql`
     query {
       dataJson {
-        storeLocator {
-          name
-          address
-          link
+        stores {
+          state
+          locations {
+            name
+            stores {
+              name
+              address
+              link
+            }
+          }
         }
       }
     }
@@ -73,34 +109,37 @@ const StoreLocatorPage = () => {
       <SEO title="Store Locator" />
       <PageTitle
         top="200"
-        bottom="140"
+        bottom="20"
         mobileTop="72"
-        mobileBottom="60"
-        title={`Store Locator`}
+        mobileBottom="0"
+        title={`Store\nLocator`}
       />
       <Box top="0">
-        {storeLocator.map((store, i) => (
-          <Store
-            key={`item-${i}`}
-            href={store.link}
-            target="_blank"
-            rel="noopener noreferrer">
-            <Grid>
-              <Grid.Unit size={11 / 12}>
-                <h2>{store.name}</h2>
-                <Text>{store.address}</Text>
-              </Grid.Unit>
-              <Grid.Unit
-                size={1 / 12}
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "flex-end",
-                }}>
-                <Arrow borderColor="#000" left="0" />
-              </Grid.Unit>
-            </Grid>
-          </Store>
+        {stores.map((state, s) => (
+          <div key={`${s}-state`}>
+            <StateName className={s === 0 ? "first" : ""}>
+              {state.state}
+            </StateName>
+            {state.locations.map((loc, l) => (
+              <LocationBlock
+                key={`${l}-loc`}
+                className={l === 0 ? "first" : ""}>
+                <LocationName>{loc.name}</LocationName>
+                <StoreList>
+                  {loc.stores.map((store, r) => (
+                    <li key={`${r}-store`}>
+                      <a
+                        href={store.link}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        {store.name}
+                      </a>
+                    </li>
+                  ))}
+                </StoreList>
+              </LocationBlock>
+            ))}
+          </div>
         ))}
       </Box>
     </>
